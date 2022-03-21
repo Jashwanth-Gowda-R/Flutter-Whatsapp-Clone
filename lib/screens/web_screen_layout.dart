23192c2c091f3158e7abccfa180abf1ea1e11114
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-03-21 15:05:13 +0530

    displaying web contacts list

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp_clone/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -8,7 +9,30 @@ class WebScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('web')),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // profile bar
+                  // search bar
+                  ContactsList()
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('images/backgroundImage.png'),
+              fit: BoxFit.cover,
+            )),
+          )
+        ],
+      ),
     );
   }
 }
